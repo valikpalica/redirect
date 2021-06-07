901714b33efa676ff10df2c6c5b89b86c3b2766d
SerhiyDemchuk <sergeylobster@gmail.com>
2021-06-07 17:01:42 +0300

    commit

--- app.js
+++ app.js
@@ -6,40 +6,38 @@ const path = require('path');
 const equal = require('./Contentfull/index');
 const domain = require('./domain.json');
 app.use(cookieParser('secret'));
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set("view engine", "hbs");
 
-
-
-app.get('/getValue',(req,res)=>{
+app.get('/getValue', (req, res) => {
     res.render('test.hbs');
 });
 
-app.get('/',(req,res)=>{
-    let hostname = req.hostname;
-    equal(hostname,req.query).then(data=>{ 
-        res.redirect(createLink(data.link,data.platform));
-    }).catch(err=>{
-        console.error(err);
-        res.status(300).send('No data');
-    });
-});
+// app.get('/',(req,res)=>{
+//     let hostname = req.hostname;
+//     equal(hostname,req.query).then(data=>{ 
+// res.redirect(createLink('localhost:8080/printouts', data.platform));
+//     }).catch(err=>{
+//         console.error(err);
+//         res.status(300).send('No data');
+//     });
+// });
 
-app.post('/',(req,res)=>{
-    let {parametrs,endpoint} = req.body;
-    equal(endpoint,parametrs).then(data=>{
-        res.redirect(createLink(data.link,data.platform));
-    }).catch(err=>{
+app.post('/', (req, res) => {
+    let { parametrs, endpoint } = req.body;
+    equal(endpoint, parametrs).then(data => {
+        res.redirect(createLink('localhost:8080/printouts', data.platform));
+    }).catch(err => {
         console.error(err);
         res.status(300).send('No data');
     })
 })
-const createLink = (endpoint,platform) =>{
+const createLink = (endpoint, platform) => {
     let port = 'http';
-    return `${port}://${endpoint}/:${platform}`;
+    return `${port}://${endpoint}?${platform}`;
 }
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server has been started on ${PORT} port`);
 })
