33c57f164183a43f34da781d2dd5bacab21f1a5f
SerhiyDemchuk <sergeylobster@gmail.com>
2021-06-11 23:25:00 +0300

    comm

--- app.js
+++ app.js
@@ -11,31 +11,18 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.set("view engine", "hbs");
 
-app.get('/getValue', (req, res) => {
-    res.render('test.hbs');
-});
-
 app.get('/',(req,res)=>{
     let hostname = req.hostname;
     equal(hostname,req.query).then(data=>{ 
-res.redirect(createLink('localhost:8080/printouts', data.platform));
+res.redirect(createLink(domain, data.platform));
     }).catch(err=>{
         console.error(err);
         res.status(300).send('No data');
     });
 });
 
-// app.post('/', (req, res) => {
-//     let { parametrs, endpoint } = req.body;
-//     equal(endpoint, parametrs).then(data => {
-//         res.redirect(createLink('localhost:8080/printouts', data.platform));
-//     }).catch(err => {
-//         console.error(err);
-//         res.status(300).send('No data');
-//     })
-// })
 const createLink = (endpoint, platform) => {
-    let port = 'http';
+    let port = 'https';
     return `${port}://${endpoint}?${platform}`;
 }
 app.listen(PORT, () => {
